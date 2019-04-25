ce77341c914f0314d9bdac6032faa9b3f8f3c541
guixiangyu <guixiangyu1@gmail.com>
2019-04-25 09:35:45 +0800

    init

--- model/ripple_model.py
+++ model/ripple_model.py
@@ -90,6 +90,7 @@ class RippleModel(BaseModel):
         all_word_ids, all_char_ids, all_word_lengths = [], [], []
         for each_words in words:
             if self.config.use_chars:
+                print(len(each_words))
                 char_ids, word_ids = zip(*each_words)  # zip参数要求是iterable即可(batch(sentence[char]))
                 word_ids, _ = pad_sequences(word_ids, 0)
                 char_ids, word_lengths = pad_sequences(char_ids, pad_tok=0,
