3ba1729811b211a769a8a144f00bd3cf23905c63
guixiangyu <guixiangyu1@gmail.com>
2019-08-31 22:30:25 +0800

    init

--- model/config.py
+++ model/config.py
@@ -71,7 +71,7 @@ class Config():
     dim_char = 100
 
     # glove files
-    filename_glove = "../spa_extractor/data/eswiki_20180420_300d.txt"
+    filename_glove = "..//eswiki_20180420_300d.txt"
     # trimmed embeddings (created from glove_filename with build_data.py)
     filename_trimmed = "data/eswiki_20180420_300d.npz"
     use_pretrained = False
